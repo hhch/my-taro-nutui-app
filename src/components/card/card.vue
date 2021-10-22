81a501fea450f9e8ff88543ff1ec5c7b8967bf0e
YYue <938778575@qq.com>
2021-10-22 16:46:11 +0800

    Card

---
+++ src/components/card/card.vue
@@ -0,0 +1,11 @@
+<template>
+  <view class="card__wrapper">
+    <view class="card__header"></view>
+    <view class="card__content"></view>
+  </view>
+</template>
+<script>
+export default {
+  name: 'MusicCard'
+}
+</script>
---
+++ src/components/card/cardItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <view></view>
+</template>
+<script>
+export default {
+  name: 'MusicCardItem'
+}
+</script>
+<template>
+  <view class="card-item__wrapper"></view>
+</template>
+<script>
+export default {
+  name: 'MusicCardItem'
+}
+</script>
