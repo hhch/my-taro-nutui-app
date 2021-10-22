1cd6133ec0f8d1707133a59a337ca5512519af89
YYue <938778575@qq.com>
2021-10-22 16:35:23 +0800

    swipe组件更新

---
+++ src/components/swiper/swiper.vue
@@ -0,0 +1,40 @@
+<template>
+  <swiper class="swiper__content" indicator-color="#eee" indicator-active-color="#0000c2" :circular="true" :autoplay="true">
+    <swiper-item v-for="(item, index) in list" :key="index">
+      <image class="swiper__image" :src="item.url" alt="" />
+    </swiper-item>
+  </swiper>
+</template>
+<script lang="ts">
+import { PropType } from 'vue'
+interface ListType {
+  url: string
+}
+export default {
+  name: 'ImageSwiper',
+  props: {
+    time: {
+      type: Number,
+      default: 3000
+    },
+    list: Array as PropType<ListType[]>
+  },
+  setup() {}
+}
+</script>
+<style lang="scss">
+.swiper__content {
+  width: 100%;
+  height: 150px;
+}
+.swiper__image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.swiper-pagination-bullet {
+  // 修改指示器样式
+  width: 10px;
+  height: 2px;
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,33 @@
 <template>
   <view class="index-wrapper">
+    <!-- input 搜索框 -->
     <search-input></search-input>
+    <!-- swiper 组件 -->
+    <image-swiper :list="data"></image-swiper>
+    <!-- 模块组件 -->
+    <TabInfo></TabInfo>
   </view>
 </template>
 
 <script>
 import { reactive, toRefs } from 'vue'
-import searchInput from '../../components/search/input.vue'
+import searchInput from '../../components/search/input.vue' // 搜索框组件
+import ImageSwiper from '../../components/swiper/swiper.vue' // 轮播图组件
+import TabInfo from '../../components/info/info.vue' // 每日菜单  等导航tab
+
 export default {
   name: 'Index',
   components: {
-    searchInput
+    searchInput,
+    ImageSwiper,
+    TabInfo
   },
-  setup() {}
+  setup() {
+    const data = [{ url: 'https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg' }, { url: 'https://storage.360buyimg.com/jdc-article/welcomenutui.jpg' }, { url: 'https://storage.360buyimg.com/jdc-article/fristfabu.jpg' }]
+    return {
+      data
+    }
+  }
 }
 </script>
 
