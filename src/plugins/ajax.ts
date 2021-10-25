906e5abc92c4972b98788bd50514c0e2760ba986
YYue <938778575@qq.com>
2021-10-25 16:58:00 +0800

    更新请求验证逻辑

--- src/plugins/ajax.ts
+++ src/plugins/ajax.ts
@@ -3,13 +3,20 @@ import Taro from '@tarojs/taro'
 const isUrl = (url: string): boolean => {
   return /^(((ht|f)tps?):\/\/)?[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-\(\)]*[\w@?^=%&/~+#-\(\)])?$/.test(url)
 }
+const cookie = Taro.getStorageSync('cookie')
+
 const baseUrl = 'http://localhost:3000'
 class Ajax {
   get(url: string, data?: object, option?: object) {
     return new Promise((resolve, reject) => {
       Taro.request({
         url: isUrl(url) ? url : baseUrl + url,
-        data,
+        data: cookie
+          ? {
+              ...data,
+              cookie
+            }
+          : data,
         method: 'GET',
         header: {},
         ...option,
@@ -26,7 +33,16 @@ class Ajax {
     return new Promise((resolve, reject) => {
       Taro.request({
         url: isUrl(url) ? url : baseUrl + url,
-        data,
+        data: cookie
+          ? {
+              ...data,
+              cookie,
+              timestamp: new Date().getTime()
+            }
+          : {
+              ...data,
+              timestamp: new Date().getTime()
+            },
         method: 'POST',
         header: {},
         ...option,
