e86d4110048c2d68473b077df1c5ffa5b56c9572
yyues <938778575@qq.com>
2021-10-31 09:59:19 +0800

    user 不是很想写

---
+++ src/components/user/userInfo.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="my-user-info__wrapper">
+    <image class="my-user-info__image" :src="data.avatarUrl"></image>
+    <view class="my-user-info__content">
+      <view class="nickname">{{ data.nickname }}</view>
+      <view class="signature">{{ data.signature }}</view>
+    </view>
+    <nut-icon name="right"></nut-icon>
+  </view>
+</template>
+<script lang="ts">
+import { PropType } from '@vue/runtime-core'
+interface ItemType {
+  nickname: string
+  avatarUrl: string
+  signature: string
+}
+export default {
+  props: {
+    data: Object as PropType<ItemType>
+  },
+  setup() {}
+}
+</script>
+<style lang="scss">
+.my-user-info__wrapper {
+  display: flex;
+  align-items: center;
+  background-color: #eee;
+  border-radius: 10px;
+  padding: 10px;
+  .my-user-info__image {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+  }
+  .my-user-info__content {
+    flex: 1;
+    font-size: 14px;
+    padding-left: 10px;
+    .nickname {
+      font-size: 14px;
+      font-weight: bolder;
+    }
+    .signature {
+      font-size: 12px;
+    }
+  }
+}
+</style>
--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -1,12 +1,41 @@
 <template>
-  <view>
-    <text>qqqq</text>
+  <view class="my-user-index__wrapper">
+    <userInfo :data="userAccount.profile" />
   </view>
 </template>
 
 <script>
+import userInfo from '../../components/user/userInfo.vue'
 export default {
-  mounted() {},
-  methods: {}
+  components: { userInfo },
+  data() {
+    return {
+      userAccount: {
+        profile: {}
+      },
+      userId: ''
+    }
+  },
+  async created() {
+    await this.getUserAccount()
+    await this.getUserLick()
+  },
+  methods: {
+    async getUserAccount() {
+      const res = await this.$ajax.get('/user/account')
+      this.userAccount = res
+      this.userId = res.profile.userId
+    },
+    async getUserLick() {
+      const res = await this.$ajax.get('/likelist', {
+        uid: this.userId
+      })
+    }
+  }
 }
 </script>
+<style lang="scss">
+.my-user-index__wrapper {
+  padding: 10px;
+}
+</style>
